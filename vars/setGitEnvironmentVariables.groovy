4b9d3128af6f4fc257697bab392e0ac3d717f983
Paul McLanahan <pmac@mozilla.com>
2017-02-23 22:47:58 -0500

    Add GIT_TAG to setGitEnvironmentVariables

--- vars/setGitEnvironmentVariables.groovy
+++ vars/setGitEnvironmentVariables.groovy
@@ -2,4 +2,9 @@ def call() {
   env.GIT_COMMIT = sh([returnStdout: true, script: 'git rev-parse HEAD']).trim()
   env.GIT_COMMIT_SHORT = env.GIT_COMMIT.take(6)
   env.GIT_BRANCH = getGitBranch()
+  try {
+    env.GIT_TAG = sh([returnStdout: true, script: 'git describe --tags --exact-match $GIT_COMMIT']).trim()
+  } catch(e) {
+    echo 'git commit not tagged'
+  }
 }
